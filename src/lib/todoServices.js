b59a8129d9278c921ba4b83f841680091e22b097
Zac Jones <zacjones93@gmail.com>
2018-05-15 13:34:06 -0700

    codesandbox support

--- src/lib/todoServices.js
+++ src/lib/todoServices.js
@@ -1,37 +1,41 @@
-const baseUrl = process.env.REACT_APP_BASE_URL
+const baseUrl = process.env.REACT_APP_BASE_URL;
+// hard-coded glitch url used for codesandbox
 
 export const getTodos = () => {
-  return fetch(baseUrl).then(res => res.json())
-}
+  return fetch("https://adventurous-kettle.glitch.me/todos", {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }).then(res => res.json());
+};
 
 export const createTodo = name => {
-  return fetch(baseUrl, {
-    method: 'POST',
+  return fetch("https://adventurous-kettle.glitch.me/todos", {
+    method: "POST",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({ name: name, isComplete: false })
-  }).then(res => res.json())
-}
+  }).then(res => res.json());
+};
 
 export const updateTodo = todo => {
-  return fetch(`${baseUrl}/${todo.id}`, {
-    method: 'PUT',
+  return fetch(`https://adventurous-kettle.glitch.me/todos/${todo.id}`, {
+    method: "PUT",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(todo)
-  }).then(res => res.json())
-}
+  }).then(res => res.json());
+};
 
 export const destroyTodo = id => {
-  return fetch(`${baseUrl}/${id}`, {
-    method: 'DELETE',
+  return fetch(`https://adventurous-kettle.glitch.me/todos/${id}`, {
+    method: "DELETE",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     }
-  })
-}
+  });
+};
